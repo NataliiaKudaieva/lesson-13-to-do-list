f8ee2dd2365510b906492872c91fbce878de2976
NataliiaKudaieva <81326193+NataliiaKudaieva@users.noreply.github.com>
2021-09-10 00:03:11 +0300

    fix

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+let inputValue = document.getElementById("name").value;
+const button = document.getElementById("myButton");
+const myList = document.querySelector("#myList");
+button.addEventListener("click", addText);
+
 function addText() {
-  let inputValue = document.getElementById("name").value;
+  inputValue = document.getElementById("name").value;
   if (!inputValue) {
     return alert("Write something!");
   }
-  document
-    .querySelector("#myList")
-    .insertAdjacentHTML("beforeend", "<div>" + inputValue + 
-    "</div>");
+  myList.insertAdjacentHTML("beforeend", "<div>" + inputValue + "</div>");
   document.getElementById("name").value = "";
 }
-
-const button = document.getElementById("myButton");
-button.addEventListener("click", addText);
